fc55ff855aa822fe81c1760b17f05c9e7f73eb8e
Woohyun Park <woohyunpark.mail@gmail.com>
2021-09-01 09:37:16 +0900

    2.6 Log OUt

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,21 +2,29 @@ import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation/>}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,16 @@
 import React from "react";
+import { auth } from "../fbase"
+import { useHistory } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+export default () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        auth.signOut();
+        history.push("/");
+    }
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+}
