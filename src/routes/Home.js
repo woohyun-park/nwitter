20a90c3bb64673d50725f6a683fcad4667001abf
Woohyun Park <woohyunpark.mail@gmail.com>
2021-09-04 18:28:47 +0900

    3.4 Delete and Update part One

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import { db } from "../fbase";
+import Nweet from "../components/Nweets";
 
 const Home = ({userObj}) => {
     const [nweet, setNweet] = useState("");
@@ -54,9 +55,11 @@ const Home = ({userObj}) => {
             </form>
             <div>
                 {nweets.map((nweet) => (
-                    <div key={nweet.id}>
-                        <h4>{nweet.text}</h4>
-                    </div>
+                    <Nweet
+                        key={nweet.id}
+                        nweetObj={nweet}
+                        isOwner={nweet.creatorId === userObj.uid}
+                    />
                 ))}
             </div>
         </div>
