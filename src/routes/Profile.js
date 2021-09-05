c9595f359472d842d35e2f1780379fb3a9441a6c
Woohyun Park <woohyunpark.mail@gmail.com>
2021-09-05 12:45:33 +0900

    5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, db } from "../fbase"
 import { useHistory } from "react-router-dom";
 
 export default ({userObj}) => {
     const history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => {
         auth.signOut();
         history.push("/");
@@ -18,8 +19,32 @@ export default ({userObj}) => {
     useEffect(() => {
         getMyNweets();
     })
+    const onChange = (event) => {
+        const value = event.target.value;
+        setNewDisplayName(value);
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if(userObj.displayName !== newDisplayName){
+            await userObj.updateProfile({
+                displayName: newDisplayName
+            });
+        }
+    }
     return (
         <>
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder="Display name"
+                onChange={onChange}
+                value={newDisplayName}
+            />
+            <input
+                type="submit"
+                value="Update Profile"    
+            />
+        </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     );
